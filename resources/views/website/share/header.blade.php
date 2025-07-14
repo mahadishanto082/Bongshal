08bacc12b247614de9f0f9ba99bfd6a45b7b4abf
mahadishanto082 <mahadishanto26@gmail.com>
2025-07-14 10:51:55 +0600

    header buttons are redesigned

--- resources/views/website/share/header.blade.php
+++ resources/views/website/share/header.blade.php
@@ -78,16 +78,14 @@
                     <div class="headd-sty-last">
                         <ul class="nav-menu nav-menu-social align-to-right align-items-center d-flex">
                         <li>
-                        <a href="{{ route('web.user.compare') }}" onclick="AddCompare()">
-
-                    
-                             <div class="d-flex align-items-center justify-content-between">
-                                 <i class="lni lni-balance fs-lg"></i><span class="dn-counter theme-bg">{{ $wishlist_count_total }}</span>
-                                                        <div class="text-left ml-1">
-                                                        
-                                                            <div class="primary-text cart-subtotal"><span class="fs-md ft-medium"><span class="prc-currency">Tk.</span>@{{ total_amount }}</span></div>
-                                                        </div>
-                                                    </div>
+                            <!-- Compare count total -->
+                            <a href="{{ route('web.user.compare') }}" onclick="AddCompare()">
+                                <div class="d-flex align-items-center justify-content-between">
+                                    <i class="lni lni-balance fs-lg"></i><span class="dn-counter theme-bg">{{ $wishlist_count_total }}</span>
+                                    <div class="text-left ml-1">
+                                        <i class="fas fa-exchange-alt text-primary"></i>
+                                    </div>
+                                </div>
                              </a>
                          </li>
 
@@ -101,7 +99,7 @@
                                  <i class="lni lni-heart fs-lg"></i><span class="dn-counter theme-bg">{{ $wishlist_count_total }}</span>
                                                         <div class="text-left ml-1">
                                                         
-                                                            <div class="primary-text cart-subtotal"><span class="fs-md ft-medium"><span class="prc-currency">Tk.</span>@{{ total_amount }}</span></div>
+                                                          
                                                         </div>
                                                     </div>
                              </a>
