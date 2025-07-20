a1c95f81327fe6e8c8aab7fa6d721b46244d325a
mahadishanto082 <mahadishanto26@gmail.com>
2025-07-20 13:51:07 +0600

    welcome-text index updated

--- resources/views/admin/welcome-text/index.blade.php
+++ resources/views/admin/welcome-text/index.blade.php
@@ -20,56 +20,64 @@
             <div class="card">
                 <div class="card-body">
                     <div class="bd bd-gray-300 rounded table-responsive">
-                        <table class="table my-table table-hover mg-b-0">
-                            <thead>
-                            <tr>
-                                <th>SL</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @if(count($welcomeTexts))
-                                @foreach($welcomeTexts as $key => $welcomeText)
-                                    <tr>
-                                        <th scope="row">{{ ++$key }}</th>
-                                        
-                                        
-                                        <td>{{ $welcomeText->title }}</td>
-                                        
-                                        <td>{{ $welcomeText->description }}</td>
-                                        <td>{{ $welcomeText->status }}</td>
-                                        <td>
-                                            <div class="dropdown">
-                                                <a class="btn btn-sm btn-outline-info dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                                                    Action
-                                                </a>
-                                                <div class="dropdown-menu">
-                                                    <a class="dropdown-item" href="{{ route('admin.WelcomeTexts.edit', $welcomeText->id) }}"><i class="fa fa-edit"></i> Edit</a>
-                                                    <a onclick="deleteRow('{{ route('admin.WelcomeTexts.destroy', $welcomeText->id) }}')" class="dropdown-item" href="javascript:void(0)"><i class="fa fa-trash"></i> Delete</a>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @else
-                                <tr>
-                                    <td colspan="8" class="text-center">
-                                        No data
-                                    </td>
-                                </tr>
-                            @endif
-                            </tbody>
-                            @if($welcomeTexts->hasPages())
-                                <tfoot>
-                                <tr>
-                                    <td colspan="8">
-                                        {{ $sliders->links('admin.shared._paginate') }}
-                                    </td>
-                                </tr>
-                                </tfoot>
-                            @endif
-                        </table>
+                    <table class="table my-table table-hover mg-b-0">
+    <thead>
+        <tr>
+            <th>SL</th>
+            <th>Title</th>
+            <th>Content</th>
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+    @if($welcomeTexts->count())
+        @foreach($welcomeTexts as $key => $welcomeText)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $welcomeText->title }}</td>
+                <td>{{ Str::limit(strip_tags($welcomeText->content), 80) }}</td>
+                <td>
+                    @if($welcomeText->status == 'Active')
+                        <span class="badge badge-success">Active</span>
+                    @else
+                        <span class="badge badge-secondary">Inactive</span>
+                    @endif
+                </td>
+                <td>
+                    <div class="dropdown">
+                        <a class="btn btn-sm btn-outline-info dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Action
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="{{ route('admin.WelcomeTexts.edit', $welcomeText->id) }}">
+                                <i class="fa fa-edit"></i> Edit
+                            </a>
+                            <a onclick="deleteRow('{{ route('admin.WelcomeTexts.destroy', $welcomeText->id) }}')" class="dropdown-item" href="javascript:void(0)">
+                                <i class="fa fa-trash"></i> Delete
+                            </a>
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+    @else
+        <tr>
+            <td colspan="5" class="text-center">No data available</td>
+        </tr>
+    @endif
+    </tbody>
+    @if($welcomeTexts->hasPages())
+        <tfoot>
+        <tr>
+            <td colspan="5">
+                {{ $welcomeTexts->links('admin.shared._paginate') }}
+            </td>
+        </tr>
+        </tfoot>
+    @endif
+</table>
+
                     </div>
                 </div>
             </div>
